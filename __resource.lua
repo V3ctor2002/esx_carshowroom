6da312134df9703a8465d6741baa89bba094ca56
Vittorio Covre <68883487+V3ctor2002@users.noreply.github.com>
2020-10-16 22:57:33 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,8 +1,11 @@
 resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
 
+description 'Developed and reworked by V3tor'
+
 client_scripts {
   '@es_extended/locale.lua',
   'translation/it.lua',
+  'translation/en.lua',
   'config.lua',
   'client/main.lua',
 }
