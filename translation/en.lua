87fa78bedba788353241c1fa023c9c05498231d8
Vittorio Covre <68883487+V3ctor2002@users.noreply.github.com>
2020-10-16 22:53:56 +0200

    Add files via upload

---
+++ translation/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+  -- Everything
+  ['press_e'] = 'Premi ~INPUT_CONTEXT~ per aprire il menu',
+  ['costs'] = ' Prezzo: ',
+  ['currency'] = ' $',
+  ['back'] = 'Torna indietro',
+  ['contact_dealer'] = 'Contatta il concessionario per il prezzo finale',
+  ['press_e_to_enter'] = 'Premi ~INPUT_CONTEXT~ per visitare l auto salone',
+  ['press_e_to_exit'] = 'Premi ~INPUT_CONTEXT~ per tornare in concessionaria',
+}
