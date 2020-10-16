a55e97bd1a84b5d598cb3ebe8a5d65b0bce104cd
Vittorio Covre <68883487+V3ctor2002@users.noreply.github.com>
2020-10-16 22:56:15 +0200

    Update en.lua

--- translation/en.lua
+++ translation/en.lua
@@ -1,10 +1,10 @@
 Locales['en'] = {
   -- Everything
-  ['press_e'] = 'Premi ~INPUT_CONTEXT~ per aprire il menu',
-  ['costs'] = ' Prezzo: ',
+  ['press_e'] = 'Press ~INPUT_CONTEXT~ to open the menu',
+  ['costs'] = ' Price: ',
   ['currency'] = ' $',
-  ['back'] = 'Torna indietro',
-  ['contact_dealer'] = 'Contatta il concessionario per il prezzo finale',
-  ['press_e_to_enter'] = 'Premi ~INPUT_CONTEXT~ per visitare l auto salone',
-  ['press_e_to_exit'] = 'Premi ~INPUT_CONTEXT~ per tornare in concessionaria',
+  ['back'] = 'Back',
+  ['contact_dealer'] = 'Contact the Cardealer for the final price',
+  ['press_e_to_enter'] = 'Press ~INPUT_CONTEXT~ to see the vehicles',
+  ['press_e_to_exit'] = 'Press ~INPUT_CONTEXT~ to go back',
 }
